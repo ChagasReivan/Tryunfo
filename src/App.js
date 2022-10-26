9e0968b4ddc7db234e03c22088be352180fc2b87
Reivan Chagas <chagasreivan@gmail.com>
2022-10-26 18:57:53 -0300

    adicioan exercicio 4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,58 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+
+  };
+
+  onInputChange = (event) => {
+    const { target } = event;
+    this.setState({
+      cardName: target.value,
+      cardDescription: target.value,
+      cardAttr1: target.value,
+      cardAttr2: target.value,
+      cardAttr3: target.value,
+      cardImage: target.value,
+      cardTrunfo: target.value,
+    });
+  };
+
   render() {
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
